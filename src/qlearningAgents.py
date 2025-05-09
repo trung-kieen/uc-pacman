1a2962ac25a68849ede7ac4373f43e3f2a22324c
trung-kieen <trungkiennk4869@gmail.com>
2025-05-09 19:08:55 +0700

    remove redundant file

--- src/qlearningAgents.py
+++ src/qlearningAgents.py
@@ -1,17 +1,3 @@
-# qlearningAgents.py
-# ------------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-#
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-
 from game import *
 from learningAgents import ReinforcementAgent
 from featureExtractors import *
@@ -26,22 +12,6 @@ import numpy as np
 import copy
 
 class QLearningAgent(ReinforcementAgent):
-    """
-      Q-Learning Agent
-      Functions you should fill in:
-        - computeValueFromQValues
-        - computeActionFromQValues
-        - getQValue
-        - getAction
-        - update
-      Instance variables you have access to
-        - self.epsilon (exploration prob)
-        - self.alpha (learning rate)
-        - self.discount (discount rate)
-      Functions you should use
-        - self.getLegalActions(state)
-          which returns legal actions for a state
-    """
     def __init__(self, **args):
         "You can initialize Q-values here..."
         ReinforcementAgent.__init__(self, **args)
@@ -60,7 +30,7 @@ class QLearningAgent(ReinforcementAgent):
           Returns max_action Q(state,action)
           where the max is over legal actions.  Note that if
           there are no legal actions, which is the case at the
-          terminal state, you should return a value of 0.0.
+          terminal state return a value of 0.0.
         """
         actions = self.getLegalActions(state)
         if len(actions) == 0:
@@ -75,10 +45,8 @@ class QLearningAgent(ReinforcementAgent):
     def computeActionFromQValues(self, state):
         """
           Compute the best action to take in a state.  Note that if there
-          are no legal actions, which is the case at the terminal state,
-          you should return None.
+          are no legal actions, which is the case at the terminal state just return None.
         """
-        "*** YOUR CODE HERE ***"
         qValuesToActions= {self.getQValue(state, action): action for action in self.getLegalActions(state)}
         max_q_values = max(qValuesToActions.keys())
         max_q_actions = [qValuesToActions[value] for value in qValuesToActions.keys() if value == max_q_values]
@@ -95,10 +63,7 @@ class QLearningAgent(ReinforcementAgent):
           Compute the action to take in the current state.  With
           probability self.epsilon, we should take a random action and
           take the best policy action otherwise.  Note that if there are
-          no legal actions, which is the case at the terminal state, you
-          should choose None as the action.
-          HINT: You might want to use util.flipCoin(prob)
-          HINT: To pick randomly from a list, use random.choice(list)
+          no legal actions, which is the case at the terminal state function return None
         """
         # Pick Action
         legalActions = self.getLegalActions(state)
@@ -113,9 +78,7 @@ class QLearningAgent(ReinforcementAgent):
         """
           The parent class calls this to observe a
           state = action => nextState and reward transition.
-          You should do your Q-Value update here
-          NOTE: You should never call this function,
-          it will be called on your behalf
+          Q-Value update here
         """
         sample = reward + self.discount * self.computeValueFromQValues(nextState)
         self.q_values[state, action] = (1 - self.alpha) * self.getQValue(state, action) + self.alpha * sample
@@ -150,8 +113,7 @@ class PacmanQAgent(QLearningAgent):
     def getAction(self, state):
         """
         Simply calls the getAction method of QLearningAgent and then
-        informs parent of action for Pacman.  Do not change or remove this
-        method.
+        informs parent of action for Pacman.
         """
         action = QLearningAgent.getAction(self,state)
         self.doAction(state,action)
@@ -159,9 +121,7 @@ class PacmanQAgent(QLearningAgent):
 
 class ApproximateQAgent(PacmanQAgent):
     """
-       ApproximateQLearningAgent
-       You should only have to overwrite getQValue
-       and update.  All other QLearningAgent functions
+       Only have to overwrite getQValue and update.  All other QLearningAgent functions
        should work as is.
     """
     def __init__(self, extractor='IdentityExtractor', **args):
@@ -177,7 +137,6 @@ class ApproximateQAgent(PacmanQAgent):
           Should return Q(state,action) = w * featureVector
           where * is the dotProduct operator
         """
-        "*** YOUR CODE HERE ***"
         feature_vector = self.featExtractor.getFeatures(state, action)
         q_value = 0.0
         for feat in feature_vector.keys():
@@ -188,9 +147,8 @@ class ApproximateQAgent(PacmanQAgent):
 
     def update(self, state, action, nextState, reward: float):
         """
-           Should update your weights based on transition
+           Update weights based on transition
         """
-        "*** YOUR CODE HERE ***"
         max_q_next_action = float("-inf")
         for next_action in self.getLegalActions(state):
             max_q_next_action = max(max_q_next_action, self.getQValue(nextState, next_action))
@@ -214,6 +172,5 @@ class ApproximateQAgent(PacmanQAgent):
 
         # did we finish training?
         if self.episodesSoFar == self.numTraining:
-            # you might want to print your weights here for debugging
-            "*** YOUR CODE HERE ***"
+            # For bugging at the end of episodes
             pass
